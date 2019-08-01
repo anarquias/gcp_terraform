d24e62bb0db10404427d23d0bf045d2591d7f7dc
Nicholas <33345295+Techdox@users.noreply.github.com>
2019-08-02 11:14:29 +1200

    Add files via upload

--- main.tf
+++ main.tf
@@ -5,25 +5,6 @@ provider "google" {
   zone        = "${var.zone_name}"
 }
 
-resource "google_compute_network" "vpc_network" {
+resource "google_compute_network" "vpc-network" {
     name = "${var.instance_network}"
-      auto_create_subnetworks = "true"
-    }
-
-resource "google_compute_instance" "vm_instance" {
-  name        = "${var.instance_name}"
-  machine_type = "${var.machine_type}"
-
-  boot_disk {
-    initialize_params {
-      image = "${var.instance_image}"
-    }
-  }
-
-  network_interface {
-    network = "${var.instance_network}"
-      access_config {
-    }
-  }
 }
-
