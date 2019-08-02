aefb7d481cfb366c04e8ed893e9e1f7d7b508776
Nicholas <33345295+Techdox@users.noreply.github.com>
2019-08-02 13:30:00 +1200

    Delete variable.tf

--- network/variable.tf
+++
@@ -1,9 +0,0 @@
-variable "instance_network" {
-  description = "Network for the instance"
-  default     = "vpc_network"
-}
-
-variable "subnetwork" {
-    description = "The name of the subnetwork to attach this interface to. The subnetwork must exist in the same region this instance will be created in. Either network or subnetwork must be provided."
-    default     = ""
-}
